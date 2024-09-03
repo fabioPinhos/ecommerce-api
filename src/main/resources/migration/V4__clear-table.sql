f2fba604c9586666074b92324b3b2bd9aaa098ee
FabioPinho <fabiopinhos@gmail.com>
2024-09-02 21:25:03 -0300

    feat: editing application.properties

--- src/main/resources/migration/V4__clear-table.sql
+++
@@ -1,7 +0,0 @@
-delete from public.flyway_schema_history where version = '1';
-delete from public.flyway_schema_history where version = '2';
-delete from public.flyway_schema_history where version = '3';
-
-DELETE FROM coupon;
-DELETE FROM address;
-DELETE FROM event;
